7ac20be9a852e1b048d84eb603980be1d7a4f1db
hakancenkalinoglu <hakancenkkalinoglu@gmail.com>
2026-04-27 18:23:08 +0300

    feat: implement memory repository with Save and GetByID methods

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-qr-order
+
+go 1.25.0
---
+++ internal/repository/memory.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"go-qr-order/internal/models"
+)
+
+type InMemoryOrderRepo struct {
+	orders map[int]models.Order
+}
+
+func NewInMemoryOrderRepo() *InMemoryOrderRepo {
+	return &InMemoryOrderRepo{
+		orders: make(map[init]models.Order),
+	}
+}
+
+func (r *InMemoryOrderRepo) Save(order models.Order) {
+	r.orders[order.ID] = order
+}
+
+func (r *InMemoryOrderRepo) GetById(id int) (models.Order, bool) {
+	order, exist := r.orders[id]
+	return order, exist
+}
